9421b71c19b2df075dcc7f350b552513b28e8865
zhouliupku <pkulz@pku.edu.cn>
2023-04-09 19:27:30 -0400

    a9 frontend

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 // const TUITS_API = 'http://localhost:4000/api/tuits'
 // const TUITS_API = 'https://tuiter-node-server-app-g4qp.onrender.com/api/tuits'
+// enable a9 deploy
+
 const API_BASE = process.env.REACT_APP_API_BASE;
 const TUITS_API = `${API_BASE}/tuits`;
 
